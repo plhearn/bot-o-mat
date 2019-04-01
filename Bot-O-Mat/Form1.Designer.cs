6ae5cdbc3efecdb54d612308a7861555cc8eb4fc
plhearn <plhearn@gmail.com>
2019-04-01 00:36:58 -0400

    initial commit

--- Bot-O-Mat/Form1.Designer.cs
+++ Bot-O-Mat/Form1.Designer.cs
@@ -28,13 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddRobot = new System.Windows.Forms.Button();
+            this.cbRobotType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRobotName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStartTask = new System.Windows.Forms.Button();
+            this.cbTask = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbRobot = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblLeaderBoard = new System.Windows.Forms.Label();
+            this.lvTasks = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSimulate = new System.Windows.Forms.Button();
+            this.btnClearHistory = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddRobot);
+            this.groupBox1.Controls.Add(this.cbRobotType);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtRobotName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(354, 216);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Robot";
+            // 
+            // btnAddRobot
+            // 
+            this.btnAddRobot.Location = new System.Drawing.Point(168, 174);
+            this.btnAddRobot.Name = "btnAddRobot";
+            this.btnAddRobot.Size = new System.Drawing.Size(109, 36);
+            this.btnAddRobot.TabIndex = 4;
+            this.btnAddRobot.Text = "Add Robot";
+            this.btnAddRobot.UseVisualStyleBackColor = true;
+            this.btnAddRobot.Click += new System.EventHandler(this.btnAddRobot_Click);
+            // 
+            // cbRobotType
+            // 
+            this.cbRobotType.FormattingEnabled = true;
+            this.cbRobotType.Location = new System.Drawing.Point(163, 129);
+            this.cbRobotType.Name = "cbRobotType";
+            this.cbRobotType.Size = new System.Drawing.Size(121, 28);
+            this.cbRobotType.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Type";
+            // 
+            // txtRobotName
+            // 
+            this.txtRobotName.Location = new System.Drawing.Point(168, 79);
+            this.txtRobotName.Name = "txtRobotName";
+            this.txtRobotName.Size = new System.Drawing.Size(116, 26);
+            this.txtRobotName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStartTask);
+            this.groupBox2.Controls.Add(this.cbTask);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cbRobot);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 261);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(396, 267);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Start Task";
+            // 
+            // btnStartTask
+            // 
+            this.btnStartTask.Location = new System.Drawing.Point(163, 207);
+            this.btnStartTask.Name = "btnStartTask";
+            this.btnStartTask.Size = new System.Drawing.Size(100, 29);
+            this.btnStartTask.TabIndex = 4;
+            this.btnStartTask.Text = "Start Task";
+            this.btnStartTask.UseVisualStyleBackColor = true;
+            this.btnStartTask.Click += new System.EventHandler(this.btnStartTask_Click);
+            // 
+            // cbTask
+            // 
+            this.cbTask.FormattingEnabled = true;
+            this.cbTask.Location = new System.Drawing.Point(109, 126);
+            this.cbTask.Name = "cbTask";
+            this.cbTask.Size = new System.Drawing.Size(262, 28);
+            this.cbTask.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Task";
+            // 
+            // cbRobot
+            // 
+            this.cbRobot.FormattingEnabled = true;
+            this.cbRobot.Location = new System.Drawing.Point(168, 60);
+            this.cbRobot.Name = "cbRobot";
+            this.cbRobot.Size = new System.Drawing.Size(121, 28);
+            this.cbRobot.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Robot";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblLeaderBoard);
+            this.groupBox3.Location = new System.Drawing.Point(12, 571);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(384, 237);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Leader Board";
+            // 
+            // lblLeaderBoard
+            // 
+            this.lblLeaderBoard.AutoSize = true;
+            this.lblLeaderBoard.Location = new System.Drawing.Point(47, 47);
+            this.lblLeaderBoard.Name = "lblLeaderBoard";
+            this.lblLeaderBoard.Size = new System.Drawing.Size(0, 20);
+            this.lblLeaderBoard.TabIndex = 0;
+            // 
+            // lvTasks
+            // 
+            this.lvTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader4,
+            this.columnHeader3});
+            this.lvTasks.Location = new System.Drawing.Point(410, 36);
+            this.lvTasks.Name = "lvTasks";
+            this.lvTasks.Size = new System.Drawing.Size(1004, 722);
+            this.lvTasks.TabIndex = 3;
+            this.lvTasks.UseCompatibleStateImageBehavior = false;
+            this.lvTasks.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Robot";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Task";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Progress";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Bar";
+            this.columnHeader3.Width = 240;
+            // 
+            // btnSimulate
+            // 
+            this.btnSimulate.Location = new System.Drawing.Point(448, 788);
+            this.btnSimulate.Name = "btnSimulate";
+            this.btnSimulate.Size = new System.Drawing.Size(141, 56);
+            this.btnSimulate.TabIndex = 4;
+            this.btnSimulate.Text = "Simulate 10 Robots";
+            this.btnSimulate.UseVisualStyleBackColor = true;
+            this.btnSimulate.Click += new System.EventHandler(this.btnSimulate_Click);
+            // 
+            // btnClearHistory
+            // 
+            this.btnClearHistory.Location = new System.Drawing.Point(698, 788);
+            this.btnClearHistory.Name = "btnClearHistory";
+            this.btnClearHistory.Size = new System.Drawing.Size(114, 56);
+            this.btnClearHistory.TabIndex = 5;
+            this.btnClearHistory.Text = "Clear Task History";
+            this.btnClearHistory.UseVisualStyleBackColor = true;
+            this.btnClearHistory.Click += new System.EventHandler(this.btnClearHistory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1445, 856);
+            this.Controls.Add(this.btnClearHistory);
+            this.Controls.Add(this.btnSimulate);
+            this.Controls.Add(this.lvTasks);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Bot-O-Mat";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddRobot;
+        private System.Windows.Forms.ComboBox cbRobotType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRobotName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStartTask;
+        private System.Windows.Forms.ComboBox cbTask;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbRobot;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblLeaderBoard;
+        private System.Windows.Forms.ListView lvTasks;
+        private System.Windows.Forms.Button btnSimulate;
+        private System.Windows.Forms.Button btnClearHistory;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
 
--- Bot-O-Mat/Form1.cs
+++ Bot-O-Mat/Form1.cs
@@ -7,14 +7,400 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Bot_O_Mat
 {
     public partial class Form1 : Form
     {
+        string strRobotTasks = @"[
+          {
+            description: 'do the dishes',
+            eta: 1000,
+          },{
+            description: 'sweep the house',
+            eta: 3000,
+          },{
+            description: 'do the laundry',
+            eta: 10000,
+          },{
+            description: 'take out the recycling',
+            eta: 4000,
+          },{
+            description: 'make a sammich',
+            eta: 7000,
+          },{
+            description: 'mow the lawn',
+            eta: 20000,
+          },{
+            description: 'rake the leaves',
+            eta: 18000,
+          },{
+            description: 'give the dog a bath',
+            eta: 14500,
+          },{
+            description: 'bake some cookies',
+            eta: 8000,
+          },{
+            description: 'wash the car',
+            eta: 20000,
+          },
+        ]";
+
+        string strRobotTypes = @"{ 
+          UNIPEDAL: 'Unipedal',
+          BIPEDAL: 'Bipedal',
+          QUADRUPEDAL: 'Quadrupedal',
+          ARACHNID: 'Arachnid',
+          RADIAL: 'Radial',
+          AERONAUTICAL: 'Aeronautical'
+        }";
+
+        public struct RobotTask
+        {
+            [JsonProperty("description")]
+            public string description;
+            [JsonProperty("eta")]
+            public int eta;
+        }
+
+        public struct RobotType
+        {
+            public string name;
+            public string description;
+        }
+
+        public class Robot
+        {
+            public string name;
+            public RobotType type;
+        }
+
+        public class TaskRecord
+        {
+            public Robot robot;
+            public RobotTask task;
+            public int completionTime;
+            public string key;
+
+            public TaskRecord(Robot robot, RobotTask task, string key)
+            {
+                this.robot = robot;
+                this.task = task;
+                this.key = key;
+                completionTime = -1;
+            }
+        }
+
+        List<Robot> robots = new List<Robot>();
+        List<RobotType> robotTypes = new List<RobotType>();
+        List<RobotTask> robotTasks = new List<RobotTask>();
+        List<TaskRecord> taskHistory = new List<TaskRecord>();
+
+        const int timerInterval = 100;
+
         public Form1()
         {
             InitializeComponent();
+
+            cbTask.DropDownStyle = ComboBoxStyle.DropDownList;  //make read only
+            cbRobot.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbRobotType.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            loadJson();
         }
+
+        private void loadJson()
+        {
+            robotTasks = JsonConvert.DeserializeObject<List<RobotTask>>(strRobotTasks);
+
+            JObject robotTypesJson = Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(strRobotTypes);
+
+            foreach (var x in robotTypesJson)
+            {
+                RobotType type = new RobotType();
+                type.name = x.Key;
+                type.description = x.Value.ToString();
+                robotTypes.Add(type);
+            }
+
+            loadDropDowns();
+        }
+
+        private void loadDropDowns()
+        {
+            loadRobotDropDown();
+
+            foreach (RobotTask task in robotTasks)
+            {
+                cbTask.DisplayMember = "Text";
+                cbTask.ValueMember = "Value";
+                cbTask.Items.Add(new { Text = task.description, Value = task });
+            }
+
+            foreach (RobotType type in robotTypes)
+            {
+                cbRobotType.DisplayMember = "Text";
+                cbRobotType.ValueMember = "Value";
+                cbRobotType.Items.Add(new { Text = type.description, Value = type });
+            }
+        }
+
+        private void loadRobotDropDown()
+        {
+            cbRobot.Items.Clear();
+
+            foreach (Robot robot in robots)
+            {
+                cbRobot.DisplayMember = "Text";
+                cbRobot.ValueMember = "Value";
+                cbRobot.Items.Add(new { Text = robot.name, Value = robot });
+            }
+        }
+
+        private void btnAddRobot_Click(object sender, EventArgs e)
+        {
+            if (txtRobotName.Text == "")
+            {
+                MessageBox.Show("You must enter a name.");
+                return;
+            }
+
+            if (cbRobotType.SelectedIndex < 0)
+            {
+                MessageBox.Show("You must select a type");
+                return;
+            }
+
+            Robot robot = new Robot();
+            robot.name = txtRobotName.Text;
+            robot.type = robotTypes[cbRobotType.SelectedIndex];
+
+            robots.Add(robot);
+
+            loadRobotDropDown();
+        }
+
+        private void btnStartTask_Click(object sender, EventArgs e)
+        {
+
+            if (cbRobot.SelectedIndex < 0)
+            {
+                MessageBox.Show("You must select a robot");
+                return;
+            }
+
+            if (cbTask.SelectedIndex < 0)
+            {
+                MessageBox.Show("You must select a task");
+                return;
+            }
+
+            Robot robot = robots[cbRobot.SelectedIndex];
+            RobotTask task = robotTasks[cbTask.SelectedIndex];
+
+            addTask(robot, task);
+        }
+
+
+        private void addTask(Robot robot, RobotTask task)
+        {
+            string key = Guid.NewGuid().ToString();     //unique identifier used for updating the progress bars
+
+            TaskRecord record = new TaskRecord(robot, task, key);
+            taskHistory.Add(record);
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += (obj, evnt) => SetProgress(key, timer);
+            timer.Interval = timerInterval;
+            timer.Enabled = true;
+
+            ListViewItem lvi = new ListViewItem();
+
+            int modifiedETA = getModifiedETA(robot.type, task);
+
+            lvi.SubItems[0].Text = robot.name;
+            lvi.SubItems.Add(task.description);
+            lvi.SubItems.Add("0 / " + modifiedETA);
+            lvi.SubItems.Add(key);
+
+            lvTasks.Items.Add(lvi);
+
+            ProgressBar pb = new ProgressBar();
+
+            pb.Minimum = 0;
+            pb.Maximum = modifiedETA;
+            pb.Value = 0;
+            pb.Name = key;
+            lvTasks.Controls.Add(pb);
+
+            updateProgressBar(key, 0);
+        }
+
+
+        private int getModifiedETA(RobotType robotType, RobotTask task)
+        {
+            float eta = task.eta;
+
+            if (robotType.name == "UNIPEDAL")  //unipedals take longer to move around the house
+            {
+                if (task.description == "sweep the house")
+                    eta *= 1.5f;
+
+                if (task.description == "take out the recycling")
+                    eta *= 1.5f;
+            }
+
+            if (robotType.name == "QUADRUPEDAL")  //four legs makes some tasks faster
+            {
+                if (task.description == "do the dishes")
+                    eta *= 0.5f;
+
+                if (task.description == "do the laundry")
+                    eta *= 0.5f;
+            }
+
+            if (robotType.name == "ARACHNID")  //spider robots are superior at everything
+                eta *= 0.75f;
+
+            if (robotType.name == "RADIAL") //radial robots are slow and not well suited for housekeeping tasks
+                eta *= 1.25f;
+
+            return (int)eta;
+        }
+
+        private int updateProgressBar(string key, int amount)
+        {
+            ProgressBar pb = lvTasks.Controls.OfType<ProgressBar>().FirstOrDefault(q => q.Name == key);
+
+            if (pb != null)
+            {
+                // find the ListViewItem based on the key
+                ListViewItem lvi = lvTasks.Items.Cast<ListViewItem>().FirstOrDefault(q => q.SubItems[3].Text == key);
+
+                //set the position of the progress bars.  doesn't work too well with scrolling.
+                Rectangle r = lvi.SubItems[3].Bounds;
+                pb.SetBounds(r.X, r.Y, r.Width, r.Height);
+
+                if (pb.Value < pb.Maximum)
+                {
+                    int newValue = pb.Value + amount;
+
+                    if (newValue > pb.Maximum)
+                        newValue = pb.Maximum;
+
+                    if (lvi != null)
+                        lvi.SubItems[2].Text = newValue.ToString() + " / " + pb.Maximum.ToString();
+
+                    pb.Value = newValue;
+                    
+                    return 1;
+                }
+                else
+                {
+                    TaskRecord record = taskHistory.First(i => i.key == key);
+                    record.completionTime = pb.Value;
+
+                    updateLeaderBoard();
+
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+
+        delegate void SetProgressCallback(string key, System.Timers.Timer timer);
+
+        private void SetProgress(string key, System.Timers.Timer timer)
+        {
+            if (this.lvTasks.InvokeRequired)
+            {
+                SetProgressCallback d = new SetProgressCallback(SetProgress);
+                this.BeginInvoke(d, new object[] { key, timer ?? null });
+            }
+            else
+            {
+                int updateResult = updateProgressBar(key, timerInterval);
+
+                if (updateResult == 0)
+                {
+                    //timer.Stop();
+                    //timer.Dispose();
+                }
+            }
+        }
+
+        private void btnClearHistory_Click(object sender, EventArgs e)
+        {
+            taskHistory.Clear();
+            lvTasks.Items.Clear();
+            lvTasks.Controls.Clear();
+            updateLeaderBoard();
+        }
+
+        private void btnSimulate_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            //create up to 10 robots
+            if (robots.Count < 10)
+            {
+                int robotCount = 10 - robots.Count;
+
+                for (int i = 0; i < robotCount; i++)
+                {
+                    Robot robot = new Robot();
+                    RobotType type = robotTypes[random.Next(robotTypes.Count)];
+
+                    robot.name = "Robot_" + i.ToString() + "_" + type.description;
+                    robot.type = type;
+
+                    robots.Add(robot);
+                }
+
+                loadRobotDropDown();
+            }
+
+
+            //assign first 10 robots to random tasks
+            for (int i = 0; i < robots.Count; i++)
+            {
+                Robot robot = robots[i];
+                RobotTask task = robotTasks[random.Next(robotTasks.Count)];
+                
+                addTask(robot, task);
+            }
+
+        }
+
+
+        private void updateLeaderBoard()
+        {
+            string strLeaderBoard = "";
+
+            foreach(Robot robot in robots)
+            {
+                strLeaderBoard += robot.name + "   ";
+
+                string strAvgTime = "N/A";
+
+                List <TaskRecord> completedRecords = taskHistory.FindAll(r => r.robot == robot && r.completionTime >= 0);
+
+                if (completedRecords.Count > 0)
+                {
+                    strAvgTime = completedRecords.Average(r => r.completionTime).ToString();
+                    strLeaderBoard += strAvgTime;
+                }
+
+                strLeaderBoard +=  "\n";
+            }
+
+            lblLeaderBoard.Text = strLeaderBoard;
+        }
+
     }
 }
